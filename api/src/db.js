6a374eadcc9bbfeb2c7d420fdaa7529e8ec2fc95
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-01-18 21:48:55 -0400

    Establish relationships between the Pokemon and Type tables using PokemonType as a junction table.

--- api/src/db.js
+++ api/src/db.js
@@ -45,6 +45,9 @@ const { Pokemon, Type } = sequelize.models;
 // Aca vendrian las relaciones
 // Product.hasMany(Reviews);
 
+Pokemon.belongsToMany(Type, { through: "PokemonType" }); // PokemonType es la tabla intermedia
+Type.belongsToMany(Pokemon, { through: "PokemonType" });
+
 module.exports = {
    ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
    conn: sequelize, // para importart la conexión { conn } = require('./db.js');
