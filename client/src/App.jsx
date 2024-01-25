68e1d83c783680af6782aa732e4117f9997c2c26
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-01-25 11:53:42 -0400

    Create NavBar.jsx component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import {Home, Landing, Detail, Form} from "./views";
 import {Route, Routes, useLocation} from 'react-router-dom';
-// import NavBar from "./components/NavBar/NavBar";
+import NavBar from "./components/NavBar/NavBar";
 
 function App() {
 const location = useLocation();
