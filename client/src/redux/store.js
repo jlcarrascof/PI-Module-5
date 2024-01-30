e17e8d8d87119bfe9f2504a9e3be985207b322c7
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-01-30 12:41:57 -0400

    Adding comments to store.js file

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -2,7 +2,15 @@ import { createStore ,applyMiddleware,compose} from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import reducer from './reducer';
 
+// This creates an enhancer to connect the app with the Redux DevTools 
+// extension in the browser.
+
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose ; //esta linea sirve para conectar nuestra app con la extension redux devtools con el navegador
+
+// This creates the Redux store using the reducer and the enhancer. 
+// The enhancer is created with applyMiddleware(thunkMiddleware), 
+// allowing to dispatch thunk functions for making requests to an API 
+// or server.
 
 const store = createStore (
     reducer,
