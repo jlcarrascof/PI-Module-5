da9c1acafad57c7ae783c8495f03104547d6b81b
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-01-30 11:51:59 -0400

    Adding comments to NavBar.jsx component

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,13 +1,17 @@
 import {Link} from "react-router-dom";
 import style from "./NavBar.module.css";
 
+// This section defines the NavBar component, which renders two links 
+// for navigation using react-router-dom.
+
 const NavBar = () => {
     return(
+        // This section renders the NavBar component.
         <div className={style.mainContainer}>
-            <Link to = "/home"  className = {style.navLink} >HOME</Link>
-            <Link to = "/create" className = {style.navLink} >FORM</Link>
+            <Link to = "/home"  className={style.navLink}>HOME</Link>
+            <Link to = "/create" className={style.navLink}>FORM</Link>
         </div>
     )
 }
 
-export default  NavBar;
+export default NavBar;
